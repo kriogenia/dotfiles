71bb230998a8ae2bfb70dd987422e63b4921ba92
Kriogenia <47500377+kriogenia@users.noreply.github.com>
2024-09-09 13:06:44 +0200

    fish: add pyenv and sdk plugins

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -7,7 +7,13 @@ abbr -a -- clr clear
 abbr -a -- nv nvim
 
 # Git
+abbr -a -- ga 'git add'
+abbr -a -- gcmsg 'git commit -m'
+abbr -a -- gs 'git status'
+abbr -a -- gss 'git status --short'
 abbr -a -- gdot '/usr/bin/git --git-dir=/home/sotoestevez/.dotfiles/ --work-tree=/home/sotoestevez'
 
-# Last
+# Fish
 abbr -a -- abbrsave 'abbr | tail -1 >> ~/.config/fish/config.fish'
+
+# Last
