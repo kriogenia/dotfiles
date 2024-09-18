c948efc51f97e6e40c9e777b1b5d34df0b9b87f2
Kriogenia <47500377+kriogenia@users.noreply.github.com>
2024-09-18 22:03:34 +0200

    Remove Code config

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -10,7 +10,10 @@ abbr -a -- tre 'exa --tree --icons'
 # Git
 abbr -a -- ga 'git add'
 abbr -a -- gcmsg 'git commit -m'
+abbr -a -- gca 'git commit --amend'
 abbr -a -- gcfu 'git commit --fixup HEAD'
+abbr -a -- gp 'git push'
+abbr -a -- gpf 'git push --force'
 abbr -a -- grfu 'git rebase -i --autosquash HEAD^^'
 abbr -a -- gs 'git status'
 abbr -a -- gss 'git status --short'
@@ -20,3 +23,4 @@ abbr -a -- gdot '/usr/bin/git --git-dir=/home/sotoestevez/.dotfiles/ --work-tree
 abbr -a -- abbrsave 'abbr | tail -1 >> ~/.config/fish/config.fish'
 
 # Last
+abbr -a -- vpip 'pyenv exec pip'
