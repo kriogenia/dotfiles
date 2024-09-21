08daf28a5149c9b8cd2a73b3cc96bfaeb1e81257
Kriogenia <47500377+kriogenia@users.noreply.github.com>
2024-09-21 20:17:26 +0200

    Fix environment variables

--- .config/fish/abbr.fish
+++ .config/fish/abbr.fish
@@ -8,6 +8,7 @@ abbr -a -- ga 'git add'
 abbr -a -- gcmsg 'git commit -m'
 abbr -a -- gca 'git commit --amend'
 abbr -a -- gcfu 'git commit --fixup HEAD'
+abbr -a -- gco 'git checkout'
 abbr -a -- gp 'git push'
 abbr -a -- gpf 'git push --force'
 abbr -a -- grfu 'git rebase -i --autosquash HEAD^^'
@@ -21,4 +22,3 @@ abbr -a -- abbrsave 'abbr | tail -1 >> ~/.config/fish/abbr.fish'
 abbr -a -- vpip 'pyenv exec pip'
 
 # Last
-abbr -a -U -- vpip 'pyenv exec pip'
--- .config/fish/fedora.fish
+++ .config/fish/fedora.fish
@@ -1,5 +1,14 @@
 # Fedora
+
+set -g DOCKER_VOLS /home/sotoestevez/Projects/.docker/volumes
+
+
+# directories
+set -g docs /home/sotoestevez/Documents
+set -g dwn /home/sotoestevez/Downloads
+set -g learning /home/sotoestevez/Projects/my_learnings
 set -g notes /home/sotoestevez/.notable/notes
+set -g projects /home/sotoestevez/Projects
 
 # Git
 abbr -a -- gdot '/usr/bin/git --git-dir=/home/sotoestevez/.dotfiles/ --work-tree=/home/sotoestevez'
