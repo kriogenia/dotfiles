57c5e7cc3770c3960527058c9972a8c9ae8670b4
SotoEstévez <ricardo@sotoestevez.dev>
2024-09-25 19:10:19 +0200

    Open CWD in Windows Terminal new pane

--- .config/fish/win.fish
+++ .config/fish/win.fish
@@ -2,3 +2,10 @@
 set -g notes /home/sotoestevez/notes/
 set -g firefox /mnt/c/Program\x20Files/Mozilla\x20Firefox/firefox.exe
 set -g BROWSER $firefox
+
+# Open CWD when creating a new pane in Windows Terminal
+function storePathForWindowsTerminal --on-variable PWD
+    if test -n "$WT_SESSION"
+        printf "\e]9;9;%s\e\\" (wslpath -w "$PWD")
+    end
+end
