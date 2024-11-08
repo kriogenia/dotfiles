32db569bad0ff808f6bc1a4edc55c7fbd6dcb537
SotoEstévez <ricardo@sotoestevez.dev>
2024-11-08 21:44:36 +0100

    Add master env vars

--- .config/fish/env_vars.fish
+++ .config/fish/env_vars.fish
@@ -1,2 +1,4 @@
 set -g todo $notes/TODO.md
 set -g EDITOR nvim
+set -g master $notes/master
+set -g rpm $learning/artificial_intelligence/rpm/
--- .config/fish/win.fish
+++ .config/fish/win.fish
@@ -1,5 +1,8 @@
 # WSL
 set -g notes /home/sotoestevez/notes/
+set -g projects /home/sotoestevez/projects/
+set -g learning /home/sotoestevez/projects/my_learnings/
+
 set -g firefox /mnt/c/Program\x20Files/Mozilla\x20Firefox/firefox.exe
 set -g BROWSER $firefox
 
