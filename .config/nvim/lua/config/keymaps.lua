28ee8e66b591efdfefa6117533dbe6dd6ee824d6
SotoEstevez <ricardo@sotoestevez.dev>
2025-03-15 21:45:54 +0100

    Remove treesitter keymap

--- .config/nvim/lua/config/keymaps.lua
+++ .config/nvim/lua/config/keymaps.lua
@@ -3,10 +3,3 @@
 
 vim.keymap.set("n", "<Tab>", ":bnext<cr>", { desc = "Next buffer" })
 vim.keymap.set("n", "<S-Tab>", ":bprevious<cr>", { desc = "Previous buffer" })
-
-vim.keymap.set(
-  "n",
-  "<leader>sx",
-  require("telescope.builtin").resume,
-  { noremap = true, silent = true, desc = "Resume" }
-)
