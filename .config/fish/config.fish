0ff28953ebbea850735e1a17080cf310f9559bd1
SotoEstevez <ricardo@sotoestevez.dev>
2025-04-04 21:14:04 +0200

    Fix tmux autostart on fish

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -12,5 +12,6 @@ pyenv init - | source
 status --is-interactive; and source (pyenv virtualenv-init -|psub)
 
 status is-interactive; and begin
+    set fish_tmux_autoquit false
     set fish_tmux_autostart true
 end
