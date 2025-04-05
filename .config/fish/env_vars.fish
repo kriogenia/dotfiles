fea098e3d3b4047eb5aa78fc90468537cb780a19
SotoEstevez <ricardo@sotoestevez.dev>
2025-04-06 01:00:40 +0200

    Unstage fish plugins

--- .config/fish/env_vars.fish
+++
@@ -1,7 +0,0 @@
-set -g _dotconfig $HOME/.config
-set -g FISH_CONF $_dotconfig/fish
-set -g NVIM_CONF $_dotconfig/nvim
-set -g TMUX_CONF $_dotconfig/tmux
-
-set -g todo $notes/TODO.md
-set -g EDITOR nvim
