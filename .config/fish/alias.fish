125c54e4a61b69fc7d5a52a21774918643c5a408
SotoEstevez <ricardo@sotoestevez.dev>
2025-04-06 15:49:30 +0200

    Change to local implementation of eza -> ls

--- .config/fish/alias.fish
+++ .config/fish/alias.fish
@@ -2,5 +2,13 @@
 alias ...='cd ../..'
 alias .3=='cd ../../..'
 
+# ls
+set EZA_OPTIONS "--color=always --group-directories-first --icons"
+alias ls="eza $EZA_OPTIONS"
+alias la="eza -al $EZA_OPTIONS"
+alias ll="eza -l $EZA_OPTIONS"
+alias lt="eza -aT $EZA_OPTIONS"
+alias l..="eza -al $EZA_OPTIONS ../"
+
 # reload fish
 alias fishr="source $__fish_config_dir/config.fish"
