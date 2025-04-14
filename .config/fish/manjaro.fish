e884b8cc50ca605cba4ef5eff27dcfd3e13e2589
SotoEstevez <ricardo@sotoestevez.dev>
2025-04-14 23:11:14 +0200

    Change to lowercase project

--- .config/fish/manjaro.fish
+++ .config/fish/manjaro.fish
@@ -2,6 +2,6 @@
 set -g docs $HOME/Documents
 set -g dwn $HOME/Downloads
 set -g notes $docs/notes/
-set -g projects $HOME/Projects
+set -g projects $HOME/projects
 
 set -g SSH_AUTH_SOCK $HOME/.bitwarden-ssh-agent.sock
