b0e4ee8a7c1149e74acb800effce357663714159
SotoEstevez <ricardo@sotoestevez.dev>
2025-04-17 10:29:18 +0200

    Transform the greeting into motd

--- .config/fish/functions/fish_greeting.fish
+++ .config/fish/functions/fish_greeting.fish
@@ -1,4 +1,6 @@
 function fish_greeting
+    status --is-login; and test $status = 0; and return
+
     set -f files (eza -1 --absolute --color=never $__fish_config_dir/greetings)
     set -f n_arts (eza -1 $__fish_config_dir/greetings | count)
     set -f pick (random 1 $n_arts)
