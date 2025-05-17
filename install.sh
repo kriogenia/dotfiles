d3c0b63195166d5c80c33c0aa40b4c68abcdef21
SotoEstevez <ricardo@sotoestevez.dev>
2025-05-17 22:09:38 +0200

    Fish set-up

--- install.sh
+++ install.sh
@@ -6,3 +6,9 @@ echo "Checking all required packages are installed"
 while read -r pkg; do
   which "$pkg"
 done <"./requirements.txt"
+
+echo "Setting up fish"
+rm -rf "$HOME/.config/fish"
+ln -s "$(pwd)/.config/fish" "$HOME/.config/fish"
+fisher install "$(cat "$HOME/.config/fish/fish_plugins")"
+
