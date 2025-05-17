d5549851e7b9aa575d00d62ae734a0156fb6d639
SotoEstevez <ricardo@sotoestevez.dev>
2025-05-17 22:34:28 +0200

    Refactor removing and linking

--- install.sh
+++ install.sh
@@ -7,14 +7,17 @@ while read -r pkg; do
   which "$pkg"
 done <"./requirements.txt"
 
-echo "Setting up fish"
-rm -rf "$HOME/.config/fish"
-ln -s "$(pwd)/.config/fish" "$HOME/.config/fish"
+echo "Overriding old configurations"
+for conf in fish tmux nvim; do
+  rm -rf "$HOME/.config/$conf"
+  ln -s "$(pwd)/.config/$conf" "$HOME/.config"
+done
+
+echo "Installing fish plugins"
 fisher install "$(cat "$HOME/.config/fish/fish_plugins")"
 
-echo "Setting up tmux"
+echo "Installing tpm plugins"
 tpm_path="$HOME/.local/share/tmux/plugins/tpm"
 git clone https://github.com/tmux-plugins/tpm "$tpm_path"
-chmod +x ./.config/tmux/scripts/*.fish
-ln -s "$(pwd)/.config/tmux" "$HOME/.config/tmux"
+chmod +x "$HOME/config/tmux/scripts/*.fish"
 "$tpm_path/bin/install_plugins.sh"
