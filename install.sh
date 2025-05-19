ee43d10ce1f88b839dc9614c127a331d78805ee6
SotoEstevez <ricardo@sotoestevez.dev>
2025-05-19 19:43:25 +0200

    fix: install script

--- install.sh
+++ install.sh
@@ -7,24 +7,28 @@ while read -r pkg; do
   which "$pkg"
 done <"./requirements.txt"
 
+config=${XDG_CONFIG_HOME:-$HOME/.config}
 echo "Overriding old configurations"
 for conf in fish tmux nvim eza kitty gh; do
-  rm -rf "$HOME/.config/$conf"
+  rm -rf "$config/${conf:?}"
   ln -s "$(pwd)/.config/$conf" "$HOME/.config"
 done
 
+echo "Installing SDKMAN"
+curl -s "https://get.sdkman.io" | bash
+
 echo "Installing fish plugins"
-fisher install "$(cat "$HOME/.config/fish/fish_plugins")"
+fish_plugins=$(tr '\n' ' ' <"$config/fish/fish_plugins")
+fish -c "fisher install $fish_plugins)"
 
 echo "Installing tpm plugins"
 tpm_path="$HOME/.local/share/tmux/plugins/tpm"
 git clone https://github.com/tmux-plugins/tpm "$tpm_path"
-chmod +x "$HOME/config/tmux/scripts/*.fish"
-"$tpm_path/bin/install_plugins.sh"
+chmod +x "$config"/tmux/scripts/*.fish
+"$tpm_path/bin/install_plugins"
 
 echo "Setting up other configs"
 mkdir "$HOME/.ssh" && ln -s "$(pwd)/.ssh/config" "$HOME/.ssh"
 ln -s "$(pwd)/.gitconfig" "$HOME/"
 
-echo "Installing SDKMAN"
-curl -s "https://get.sdkman.io" | bash
+echo "Dotfiles installed"
