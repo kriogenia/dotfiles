ede31b22b158e2529298578c723e86e28964c667
SotoEstevez <ricardo@sotoestevez.dev>
2025-05-23 00:42:42 +0200

    Remove hub and bsky sections in dashboard

--- .config/nvim/lua/plugins/dashboard.lua
+++ .config/nvim/lua/plugins/dashboard.lua
@@ -19,13 +19,6 @@ return {
           { section = "header" },
           {
             pane = 2,
-            section = "terminal",
-            cmd = "bsky tl -n 2",
-            height = 5,
-            padding = 1,
-          },
-          {
-            pane = 2,
             icon = " ",
             desc = "Browse Repo",
             padding = 1,
@@ -37,20 +30,6 @@ return {
           { section = "keys", gap = 1, padding = 1 },
           { pane = 2, icon = " ", title = "Recent Files", section = "recent_files", indent = 2, padding = 1 },
           { pane = 2, icon = " ", title = "Projects", section = "projects", indent = 2, padding = 1 },
-          {
-            pane = 2,
-            icon = " ",
-            title = "Git Status",
-            section = "terminal",
-            enabled = function()
-              return Snacks.git.get_root() ~= nil
-            end,
-            cmd = "hub status --short --branch --renames",
-            height = 5,
-            padding = 1,
-            ttl = 5 * 60,
-            indent = 3,
-          },
           { section = "startup" },
         },
       },
