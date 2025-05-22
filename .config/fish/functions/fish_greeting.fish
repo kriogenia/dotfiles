ac1eb8727650f5fe06be81a1f97d0298b91d4b94
SotoEstevez <ricardo@sotoestevez.dev>
2025-05-23 00:27:42 +0200

    Remove fish greetings

--- .config/fish/functions/fish_greeting.fish
+++ .config/fish/functions/fish_greeting.fish
@@ -1,10 +1,2 @@
 function fish_greeting
-    status --is-login; and test $status = 0; and return
-
-    set -f files (eza -1 --absolute --color=never $__fish_config_dir/greetings)
-    set -f n_arts (eza -1 $__fish_config_dir/greetings | count)
-    set -f pick (random 1 $n_arts)
-
-    test (random 0 99) = 0; and set -l cat lolcat; or set -l cat cat
-    $cat (eza -1 --absolute --color=never $__fish_config_dir/greetings | take $pick | tail -1)
 end
