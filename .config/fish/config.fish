757047059b9ed8da53bd4196d3ca9c6b9d773865
SotoEstevez <ricardo@sotoestevez.dev>
2025-06-24 19:59:09 +0200

    transform the whole machine dependent fish thing

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,12 +1,5 @@
-function read_os
-    test -d /mnt/c/ \
-        && echo win \
-        || cat /etc/os-release | rg "^ID=" | sed 's/ID=//'
-end
-
-set -f os_config $__fish_config_dir/(read_os).fish
-test -f $os_config; and source $os_config
-source $__fish_config_dir/common.fish
+source $__fish_config_dir/default.fish
+source $__fish_config_dir/override.fish
 
 pyenv init - | source
 
--- .config/fish/manjaro.fish
+++
@@ -1,7 +0,0 @@
-# Manjaro
-set -g docs $HOME/Documents
-set -g dwn $HOME/Downloads
-set -g notes $docs/notes/
-set -g projects $HOME/projects
-
-set -g SSH_AUTH_SOCK $HOME/.bitwarden-ssh-agent.sock
