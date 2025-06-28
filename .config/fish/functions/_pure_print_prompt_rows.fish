72ec8097f5036735722d32c4b27ae8e1b712d785
SotoEstevez <ricardo@sotoestevez.dev>
2025-06-28 21:42:35 +0200

    move from keeping plugins to download on bootstrap

--- .config/fish/functions/_pure_print_prompt_rows.fish
+++
@@ -1,6 +0,0 @@
-function _pure_print_prompt_rows \
-    --description "Manage default vs. compact prompt"
-
-    # print current path, git branch/status, command duration
-    echo -e -n (_pure_prompt_first_line)
-end
--- .config/fish/functions/_pure_prompt_new_line.fish
+++
@@ -1,6 +0,0 @@
-function _pure_prompt_new_line \
-    --description "Do not add a line break to a brand new session" \
-    --on-event fish_prompt
-
-    echo -e -n (_pure_prompt_beginning)
-end
