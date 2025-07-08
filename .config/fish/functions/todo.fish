d7cbb06156984bab9b18f09a83c64d300f044337
SotoEstevez <ricardo@sotoestevez.dev>
2025-07-09 00:41:03 +0200

    new todo wrapping task warrior

--- .config/fish/functions/todo.fish
+++ .config/fish/functions/todo.fish
@@ -1,12 +1,14 @@
 function todo \
-    --description "Eases operations with the TODO document" \
-    --argument-names action
-    switch $action
-        case open
-            fish -c "cd $notes && nvim TODO.md"
-        case add
-            echo "1." $argv[2..] >>"$notes/TODO.md"
-        case show '*'
-            glow "$notes/TODO.md"
+    --description "Wraps task ensuring automatic export"
+
+    if [ $argv[1] = push ] # 'push' rebases the content before pushing
+        git -C $notes rebase --autosquash origin/HEAD^
+        git -C $notes push --force
+        return
     end
+
+    task $argv
+    task status:pending export >$notes/tasks.json
+    git -C $notes add tasks.json
+    git -C $notes commit -q --allow-empty-message --fixup=origin/HEAD &>/dev/null
 end
