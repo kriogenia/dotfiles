0940d40d011b625addb84b32348677e80db3f841
SotoEstevez <ricardo@sotoestevez.dev>
2025-07-08 14:48:57 +0200

    fix pyenv not being conditional

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,16 +1,18 @@
 source $__fish_config_dir/default.fish
 source $__fish_config_dir/override.fish
 
-pacman -Q pyenv; and pyenv init - | source
-
 status is-interactive; and begin
 
     source $__fish_config_dir/alias.fish
     source $__fish_config_dir/abbr.fish
     set -x MANPAGER "nvim +Man!"
 
-    pyenv virtualenv-init - &>/dev/null
-    test $status = 0; and source (pyenv virtualenv-init -|psub)
+    if pacman -Q pyenv &>/dev/null
+        pyenv init - | source
+        if pyenv virtualenv-init - &>/dev/null
+            source (pyenv virtualenv-init -|psub)
+        end
+    end
 
     # CTRL-t = fzf select
     # CTRL-r = fzf history
