97ac1a8d584bb0610cccfffd10ab6641219bb907
SotoEstevez <ricardo@sotoestevez.dev>
2025-07-09 13:54:07 +0200

    todo pull

--- .config/fish/functions/todo.fish
+++ .config/fish/functions/todo.fish
@@ -1,14 +1,26 @@
 function todo \
     --description "Wraps task ensuring automatic export"
-
-    if [ (count $argv) = 1 ]; and [ $argv[1] = push ] # `push` rebases the content before pushing
-        git -C $notes rebase --autosquash origin/HEAD^
-        git -C $notes push --force
-        return
+    set -f git git -C $notes
+    if [ (count $argv) = 1 ]
+        if [ $argv[1] = push ]
+            $git rebase --autosquash origin/HEAD^
+            $git push --force
+            return
+        end
+        if [ $argv[1] = pull ]
+            set -l pull_output ($git pull)
+            if echo $pull_output | grep -q 'Already up to date.'
+                echo "Already up to date"
+            else
+                task import $notes/tasks.json
+                echo "New tasks found, imported"
+            end
+            return
+        end
     end
 
     task $argv
     task status:pending export >$notes/tasks.json
-    git -C $notes add tasks.json
-    git -C $notes commit -q --allow-empty-message --fixup=origin/HEAD &>/dev/null
+    $git add tasks.json
+    $git commit -q --allow-empty-message --fixup=origin/HEAD &>/dev/null
 end
