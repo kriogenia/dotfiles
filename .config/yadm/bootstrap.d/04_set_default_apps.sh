fe0c5dd4cb32c38b5b2fa4a166d414cea3da595a
SotoEstevez <ricardo@sotoestevez.dev>
2025-08-21 02:08:42 +0200

    forgot some print messages

--- .config/yadm/bootstrap.d/04_set_default_apps.sh
+++ .config/yadm/bootstrap.d/04_set_default_apps.sh
@@ -3,6 +3,7 @@
 update-desktop-database ~/.local/share/applications
 
 if [ -f /usr/bin/imv ]; then
+  echo "Setting imv as default app for images"
   xdg-mime default imv.desktop image/png
   xdg-mime default imv.desktop image/jpeg
   xdg-mime default imv.desktop image/gif
@@ -12,16 +13,19 @@ if [ -f /usr/bin/imv ]; then
 fi
 
 if [ -f /usr/bin/zathura ]; then
+  echo "Setting zathura as default app for pdfs"
   xdg-mime default zathura.desktop application/pdf
 fi
 
 if [ -f /usr/bin/qutebrowser ]; then
+  echo "Setting qutebrowser as default web browser"
   xdg-settings set default-web-browser qutebrowser.desktop
   xdg-mime default qutebrowser.desktop x-scheme-handler/http
   xdg-mime default qutebrowser.desktop x-scheme-handler/https
 fi
 
 if [ -f /usr/bin/mpv ]; then
+  echo "Setting mpv as default app for videos"
   xdg-mime default mpv.desktop video/mp4
   xdg-mime default mpv.desktop video/x-msvideo
   xdg-mime default mpv.desktop video/x-matroska
