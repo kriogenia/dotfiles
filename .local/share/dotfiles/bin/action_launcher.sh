6098cbb8b1753f4d32136c9f0675a7350152dc4f
SotoEstevez <ricardo@sotoestevez.dev>
2025-08-25 15:33:14 +0200

    launch apps and hypr shortcut

--- .local/share/dotfiles/bin/action_launcher.sh
+++ .local/share/dotfiles/bin/action_launcher.sh
@@ -7,11 +7,34 @@ PLACEHOLDER=$(yq -r ".placeholder" <"$LAUNCHER_FILE")
 option=$(yq -r ".options.[].text" "$LAUNCHER_FILE" | walker --dmenu --placeholder "$PLACEHOLDER")
 content=$(yq ".options.[] | select(.text == \"$option\")" "$LAUNCHER_FILE")
 
-# function get_prop
-# function open webpage
+launch() {
+  "${XDG_DATA_HOME}"/dotfiles/bin/action_launcher.sh "$@"
+}
 
-case $(echo "$content" | yq -r ".type") in
-*launcher*) "$(pwd)"/action_launcher.sh "$(echo "$content" | yq -r ".name")" ;;
-*url*) xdg-open "$(echo "$content" | yq -r ".url")" ;; # open in new instance?
-*) echo "Unknown option" ;;
+run_app() {
+  exec setsid uwsm app -- "$1"
+}
+
+get_prop() {
+  local prop=$1
+  echo "$content" | yq -r ".$prop"
+}
+
+return_to_start() {
+  if [ "$(basename "$LAUNCHER_FILE")" != "start.yml" ]; then
+    launch
+  fi
+}
+
+case "$(get_prop type)" in
+*app*)
+  run_app "$(get_prop exec)"
+  ;;
+*launcher*)
+  launch "$(get_prop name)"
+  ;;
+*url*)
+  xdg-open "$(get_prop url)"
+  ;;
+*) return_to_start ;;
 esac
