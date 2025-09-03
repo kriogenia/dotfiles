edf39ca3ea8a87e4683ff7f50673caba7d3e9691
SotoEstevez <ricardo@sotoestevez.dev>
2025-09-03 02:43:15 +0200

    simplify current palette

--- .config/rofi/scripts/theme.sh
+++ .config/rofi/scripts/theme.sh
@@ -6,4 +6,7 @@ if [ -n "$1" ]; then
 fi
 
 echo -en "\0markup-rows\x1ftrue\n"
-awk '{gsub(";","",$3); printf "%s\0meta\x1f%s\x1fdisplay\x1f<span foreground=\"%s\"></span> %s\n", $3, $2, $3, $2}' "$DOTFILES_DATA"/palette/current.css
+
+while IFS='=' read -r name value; do
+  printf "%s\0meta\x1f%s\x1fdisplay\x1f<span foreground=\"%s\"></span> %s\n" "$value" "$name" "$value" "$name"
+done <"$DOTFILES_DATA"/palette/current
