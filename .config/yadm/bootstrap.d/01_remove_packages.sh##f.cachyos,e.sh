afd8e56dd5f342c98a8d173f9593ab46df7c3eff
SotoEstevez <ricardo@sotoestevez.dev>
2025-09-05 16:59:52 +0200

    fix package removal

--- .config/yadm/bootstrap.d/01_remove_packages.sh##f.cachyos,e.sh
+++ .config/yadm/bootstrap.d/01_remove_packages.sh##f.cachyos,e.sh
@@ -1,17 +1,25 @@
 #!/usr/bin/env bash
 
+unwanted=(
+  alacritty
+  cachyos-alacritty-config
+  cachyos-fish-config
+  cachyos-hyprland-settings
+  mako
+  micro
+  nano
+  rofi
+  rofi-emoji
+  swaylock-fancy-git
+  wofi
+)
+to_remove=$(pacman -Qq "${unwanted[@]}" 2>/dev/null)
+
 # easier to do this than manually uncheck in the Calamares installer
 echo "Removing packages bundled not wanted"
-sudo pacman -R --noconfirm \
-  alacritty \
-  cachyos-alacritty-config \
-  cachyos-hyprland-settings \
-  mako \
-  micro \
-  nano \
-  swaylock-fancy-git \
-  wofi \
-  &>/dev/null
+echo "$to_remove"
+[ -n "$to_remove" ] && sudo pacman -Runs --noconfirm $to_remove
+# &>/dev/null
 
 if [ "$(hostname)" = umbreon ]; then
   sudo pacman -R --noconfirm brightnessctl &>/dev/null
