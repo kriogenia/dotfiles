b0a2f9e5fd482329118643c597d6e5cca565c3a9
Ric Soto <rsoto@ravenpack.com>
2025-09-22 08:27:52 +0200

    install snaps

--- .config/yadm/bootstrap.d/20_install_packages.sh##d.Ubuntu,e.sh
+++ .config/yadm/bootstrap.d/20_install_packages.sh##d.Ubuntu,e.sh
@@ -5,3 +5,9 @@ pkgs=$HOME/.local/state/dotfiles
 
 echo "Installing APT packages"
 xargs -a "$pkgs/apt.lst" sudo apt install -y
+
+echo "Installing snaps"
+while IFS= read -r line || [ -n "$line" ]; do
+  [[ -z "$line" || "$line" =~ ^# ]] && continue
+  sudo snap install "$line"
+done <"$pkgs/snap.lst"
