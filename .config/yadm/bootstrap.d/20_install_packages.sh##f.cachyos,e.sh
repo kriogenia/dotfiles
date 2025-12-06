7214811e19622a3638b575623423e287a36df274
SotoEstevez <ricardo@sotoestevez.dev>
2025-12-06 13:31:46 +0100

    missing aur installing for cachy

--- .config/yadm/bootstrap.d/20_install_packages.sh##f.cachyos,e.sh
+++ .config/yadm/bootstrap.d/20_install_packages.sh##f.cachyos,e.sh
@@ -5,3 +5,6 @@ pkgs=$HOME/.local/state/dotfiles
 
 echo "Installing Arch packages"
 sudo pacman --noconfirm --needed -S $(cat $pkgs/pacman.lst)
+
+echo "Installing AUR packages"
+paru --noconfirm -S $(cat $pkgs/aur.lst)
