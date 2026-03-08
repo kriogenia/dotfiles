b1154471f5f748064a5390218b4a129e4c2e55a9
SotoEstevez <ricardo@sotoestevez.dev>
2026-03-08 13:07:17 +0100

    so the cachyos override was never working

--- .config/fish/override.fish##d.cachyos,e.fish
+++ .config/fish/override.fish##d.cachyos,e.fish
@@ -2,6 +2,9 @@ set -g BROWSER qutebrowser
 
 set -g PACKAGE_TEMPLATE $XDG_STATE_HOME/dotfiles/pacman.lst##template
 
+set -gx TORRENT_DIR $HOME/Downloads/torrent
+set -gx MEDIA_SERIES /srv/media/series
+
 function _check_pkg_installed -d "OS specific package installation check" \
     -a package
     pacman -Q $package &>/dev/null
