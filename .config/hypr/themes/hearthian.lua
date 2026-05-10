5280832bc192eb56f8bae532f293dae9a530bb34
SotoEstevez <ricardo@sotoestevez.dev>
2026-05-10 21:46:12 +0200

    hearthian theme

---
+++ .config/hypr/themes/hearthian.lua
@@ -0,0 +1,70 @@
+-- Custom theme based on Outer Wilds
+
+local purple = "#6a4d75"
+local purpleAlpha = "#6a4d75"
+
+local indigo = "#2a1b40"
+local indigoAlpha = "#2a1b40"
+
+local darkblue = "#161a38"
+local darkblueAlpha = "#161a38"
+
+local blue = "#174e7d"
+local blueAlpha = "#174e7d"
+
+local cyan = "#8ac4d9"
+local cyanAlpha = "#8ac4d9"
+
+local green = "#676e4c"
+local greenAlpha = "#676e4c"
+
+local yellow = "#f4aa51"
+local yellowAlpha = "#f4aa51"
+
+local orange = "#ff7d25"
+local orangeAlpha = "#ff7d25"
+
+local brown = "#734434"
+local brownAlpha = "#734434"
+
+local red = "#8c301b"
+local redAlpha = "#8c301b"
+
+local white = "#f5efd5"
+local whiteAlpha = "#f5efd5"
+
+local text = "#cdd6f4"
+local textAlpha = "#cdd6f4"
+
+local subtext1 = "#bac2de"
+local subtext1Alpha = "#bac2de"
+
+local subtext0 = "#a6adc8"
+local subtext0Alpha = "#a6adc8"
+
+local overlay2 = "#9399b2"
+local overlay2Alpha = "#9399b2"
+
+local overlay1 = "#7f849c"
+local overlay1Alpha = "#7f849c"
+
+local overlay0 = "#6c7086"
+local overlay0Alpha = "#6c7086"
+
+local surface2 = "#585b70"
+local surface2Alpha = "#585b70"
+
+local surface1 = "#45475a"
+local surface1Alpha = "#45475a"
+
+local surface0 = "#313244"
+local surface0Alpha = "#313244"
+
+local base = "#0b1e26"
+local baseAlpha = "#0b1e26"
+
+local mantle = "#091b22"
+local mantleAlpha = "#091b22"
+
+local crust = "#010303"
+local crustAlpha = "#010303"
