fb04878b90b5a807f914f7b2e81bafe89c214d54
SotoEstevez <ricardo@sotoestevez.dev>
2026-05-12 14:09:38 +0200

    lets change things a bit

--- .config/hypr/autostart.lua
+++
@@ -1,9 +0,0 @@
-require("./alt/autostart.conf")
-
-hl.on("hyprland.start", function()
-	hl.exec_cmd(idle_handler .. " &")
-	hl.exec_cmd("swaybg -o * -i $HOME/.local/share/dotfiles/wallpapers/ow_ultrawide_macchiato.jpg -m fill")
-	hl.exec_cmd("wl-paste --watch cliphist store &")
-	hl.exec_cmd(notification_daemon .. "&")
-	hl.exec_cmd(terminal)
-end)
