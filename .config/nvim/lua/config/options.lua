522b2fdb26e3391e3c510c121ff151d26d081480
SotoEstevez <ricardo@sotoestevez.dev>
2026-06-25 22:59:35 +0200

    use ty

--- .config/nvim/lua/config/options.lua
+++ .config/nvim/lua/config/options.lua
@@ -4,7 +4,8 @@
 -- Python
 
 -- vim.g.lazyvim_python_lsp = "pyrefly"
-vim.g.lazyvim_python_lsp = "pyright"
+-- vim.g.lazyvim_python_lsp = "pyright"
+vim.g.lazyvim_python_lsp = "ty"
 -- vim.g.lazyvim_python_lsp = "basedpyright"
 vim.g.lazyvim_python_ruff = "ruff"
 
